1a759adc0f26f68891c0256ac6afe9ef1305ec8b
Marcus Petit <marcuspetit@hotmail.com>
2024-07-09 20:50:19 -0300

    atualizar arquivos

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -19,7 +19,7 @@
 
     <div class ="conteudo-pagina">
         <div class="titulo-pagina-2">
-            <p>Listar</p>
+            <p>Fornecedor Listar</p>
         </div>
 
         <div class="menu">
@@ -30,6 +30,43 @@
 
         </div>
 
-        LIstar....
+        <div class = 'informacao-pagina'>
+            <div class = " width: 90%; margin-left: margin-rigt: auto;">
+
+
+                <table border='1' width=100%>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Site</th>
+                            <th>UF</th>
+                            <th>Email</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                        @foreach ($fornecedores as $fornecedor)
+                            <tr>
+                                <td>{{ $fornecedor->nome }}</td>
+                                <td>{{ $fornecedor->site }}</td>
+                                <td>{{ $fornecedor->uf }}</td>
+                                <td>{{ $fornecedor->email }}</td>
+                                <td>{{ $fornecedor->nome }}</td>
+                                <td>Excluir</td>
+                                <td>Editar</td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+
+
+            </div>
+        </div>
+
+
     </div>
 @endsection
